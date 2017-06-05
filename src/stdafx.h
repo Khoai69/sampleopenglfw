26a6a9119457b846a07995b59f3507df6f04198d
Khoai <khanh.hoangbao@outlook.com>
2017-06-05 22:46:20 +0700

    + initual

---
+++ src/stdafx.h
@@ -0,0 +1,16 @@
+//std things
+#include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+
+//glm
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+//assimp
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
